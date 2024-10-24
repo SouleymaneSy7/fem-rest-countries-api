6f849cedadc057e29a6be6d34b3c956166e893f2
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-24 16:08:42 +0000

    config: update the tailwindcss, add the (font-family, font-size, colors...) configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,27 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    colors: {
+      "color-elements": "var(--color-elements)",
+      "color-background": "var(--color-background)",
+      "color-input-text": "var(--color-input-text)",
+      "color-text": "var(--color-text)",
+    },
+    fontWeight: {
+      "fw-bold": "var(--fw-bold)",
+      "fw-semi-bold": "var(--fw-semi-bold)",
+      "fw-regular": "var(--fw-regular)",
+    },
     extend: {},
   },
   plugins: [],
