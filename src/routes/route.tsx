c56f671ba071a95382baad107fb439bfcef05fdc
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-25 23:59:23 +0000

    feat: add the react router configs

---
+++ src/routes/route.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "../pages/Home";
+import CountryPage from "../pages/CountryPage";
+import ErrorsPage from "../pages/ErrorsPage";
+
+const RouterConfig: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/:name"} element={<CountryPage />} />
+        <Route path={"*"} element={<ErrorsPage />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
+
+export default RouterConfig;
