a9384be57a60a34f1e1b5ba2b796ec365de0fc77
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-25 19:29:57 +0000

    feat: add the single country get request

--- src/store/CountryStore.ts
+++ src/store/CountryStore.ts
@@ -36,6 +36,7 @@ type CountriesDataType = {
 
 type StateType = {
   viewState: ViewStateTypes;
+  country: CountriesDataType | null;
   countries: CountriesDataType[];
 };
 
@@ -43,10 +44,12 @@ type ActionsType = {
   getCountriesData: () => void;
   getCountriesByName: (countryName: string) => void;
   getCountriesByRegion: (regionName: string) => void;
+  getSingleCountry: (name: string) => void;
 };
 
 const useCountryStore = create<StateType & ActionsType>()((set) => ({
   viewState: { status: "loading" },
+  country: null,
   countries: [],
   getCountriesData: async () => {
     try {
@@ -99,6 +102,26 @@ const useCountryStore = create<StateType & ActionsType>()((set) => ({
       } as AxiosRequestConfig);
 
       set(() => ({ countries: response.data }));
+
+      set(() => ({ viewState: { status: "done" } }));
+    } catch (error) {
+      if (error instanceof Error) {
+        set(() => ({ viewState: { status: "errors" } }));
+        console.error(error);
+      }
+    }
+  },
+  getSingleCountry: async (name) => {
+    try {
+      set(() => ({ viewState: { status: "loading" } }));
+
+      const response: AxiosResponse<CountriesDataType> = await axios({
+        method: "GET",
+        url: `https://restcountries.com/v3.1/name/${name}`,
+        responseType: "json",
+      } as AxiosRequestConfig);
+
+      set(() => ({ country: response.data }));
 
       set(() => ({ viewState: { status: "done" } }));
     } catch (error) {
