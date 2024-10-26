3e67738488446e0635a9de434bd7e1b21bf8c42c
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-26 00:01:03 +0000

    feat: add the pages components

---
+++ src/pages/CountryPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryPage: React.FC = () => {
+  return <div>CountryPage</div>;
+};
+
+export default CountryPage;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Countries from "../components/Countries";
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <Countries />
+    </main>
+  );
+};
+
+export default Home;
