9b0bd927f140b88919c7dc09b25833042573d78d
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-26 17:02:57 +0000

    feat: add the countries Data from the API

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import useCountryStore from "../store/CountryStore";
+import CountryItem from "./CountryItem";
+import { Link } from "react-router-dom";
 
 const Countries: React.FC = () => {
-  return <div>Countries</div>;
+  const countries = useCountryStore((state) => state.countries);
+  const getCountriesData = useCountryStore((state) => state.getCountriesData);
+
+  React.useEffect(() => {
+    getCountriesData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <section>
+      {countries.map((country) => {
+        return (
+          <React.Fragment key={country.cca3}>
+            <Link to={`/${country.name?.common}`}>
+              <CountryItem
+                imgSrc={country.flags.svg}
+                title={country.name?.common}
+                population={country.population}
+                region={country.region}
+                capital={country.capital}
+              />
+            </Link>
+          </React.Fragment>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Countries;
