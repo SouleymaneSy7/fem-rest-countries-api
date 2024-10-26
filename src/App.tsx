dd426e9b72f90b4fe2a753a5cb842852c531bec5
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-26 12:20:18 +0000

    feat: add the Header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import RouterConfig from "./routes/route";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
+      <Header />
       <RouterConfig />
     </React.Fragment>
   );
