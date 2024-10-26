c61fee38dfb158a30795cef46403483feae80e2e
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-26 12:19:55 +0000

    feat: add the header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Title from "./Title";
+import Buttons from "./Buttons";
+
+const Header: React.FC = () => {
+  return (
+    <header className="py-5 container border-b border-b-color-text">
+      <nav className="flex items-center justify-between">
+        <Link to="/">
+          <Title level="h1">Where in the world?</Title>
+        </Link>
+
+        <Buttons type={"button"} className="flex items-center gap-3">
+          <i className="fa-regular fa-moon"></i>
+          <span>Dark Mode</span>
+        </Buttons>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
