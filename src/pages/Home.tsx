9ea55ff45f6d28dc065c8c415854cc219be3a7f7
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-26 20:18:45 +0000

    feat: update the home components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import CountryFilterByRegion from "../components/CountryFilterByRegion";
 
 const Home: React.FC = () => {
   return (
-    <main className="container py-4">
-      <section className="flex flex-col gap-4 md:flex-row md:justify-between md:items-center mb-5">
+    <main className="container py-4 mt-5 md:mt-6">
+      <section className="flex flex-col gap-10 md:flex-row md:justify-between md:items-center mb-9 md:mb-11">
         <CountrySearchInput />
         <CountryFilterByRegion />
       </section>
