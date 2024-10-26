ff8c170793752c1b61e14eb95559a5f57d1943d7
SouleymaneSy7 <souleymanesyservices@gmail.com>
2024-10-26 13:04:36 +0000

    feat: add the country list item data

---
+++ src/components/CountryItem.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Title from "./Title";
+
+type CountryItemPropsType = {
+  imgSrc: string;
+  title: string;
+  population: number;
+  region: string;
+  capital: string;
+};
+
+const CountryItem: React.FC<CountryItemPropsType> = ({
+  imgSrc,
+  title,
+  population,
+  region,
+  capital,
+}) => {
+  return (
+    <article>
+      <img src={imgSrc} alt={`${title} flags picture`} />
+
+      <div>
+        <Title level="h2">{title}</Title>
+
+        <ul>
+          <li>Population: {population}</li>
+          <li>Region: {region}</li>
+          <li>Capital: {capital}</li>
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default CountryItem;
