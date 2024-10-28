fbf8d1ef05571818bec466fd74bc97cf54284dc5
Souleymane Sy <110754587+SouleymaneSy7@users.noreply.github.com>
2024-10-28 11:14:20 +0000

    fix: fix the listFormat errors

--- src/helpers/formatList.ts
+++ src/helpers/formatList.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// @ts-nocheck
+
 const formatter = (array: string[], type: string): string => {
   return new Intl.ListFormat("en-GB", {
     style: "long",
